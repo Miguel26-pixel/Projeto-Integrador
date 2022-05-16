8d3ef5253668cfc7d5c875a9e433068779b65d18
Nuno Alves <nyansword@gmail.com>
2022-05-16 18:17:48 +0100

    maintenance: formatting

--- rpi/server_connect/core.py
+++ rpi/server_connect/core.py
@@ -1,57 +1,59 @@
 from server_connect.utils import get_url, check_in_config
-import os
 import requests
 import time
-	
+
+
 def general_request(request_func):
-	remaining_tries = check_in_config("CONNECTION_TRIES")
-	try:
-		remaining_tries = int(remaining_tries)
-	except:
-		remaining_tries = -1
-	
-	timeout_inc = check_in_config("CONNECTION_TIMEOUT_INC")
-	try:
-		timeout_inc = int(timeout_inc)
-	except:
-		timeout_inc = 2
-		
-	timeout = check_in_config("CONNECTION_TIMEOUT")
-	try:
-		timeout = int(timeout)
-	except:
-		timeout = 5
-		
-	if remaining_tries == -1:
-		condition = lambda: True
-	else:
-		condition = lambda: remaining_tries > 0
-		
-	while condition():
-		try:
-			return request_func()
-		except:
-			print("Couldn't connect. Trying again in {} seconds.".format(timeout))
-			time.sleep(timeout)
-			timeout = timeout + timeout_inc
-			remaining_tries = remaining_tries - 1
-			
-	raise Error()
+    remaining_tries = check_in_config("CONNECTION_TRIES")
+    try:
+        remaining_tries = int(remaining_tries)
+    except:
+        remaining_tries = -1
+
+    timeout_inc = check_in_config("CONNECTION_TIMEOUT_INC")
+    try:
+        timeout_inc = int(timeout_inc)
+    except:
+        timeout_inc = 2
+
+    timeout = check_in_config("CONNECTION_TIMEOUT")
+    try:
+        timeout = int(timeout)
+    except:
+        timeout = 5
+
+    if remaining_tries == -1:
+        def condition(): return True
+    else:
+        def condition(): return remaining_tries > 0
+
+    while condition():
+        try:
+            return request_func()
+        except:
+            print("Couldn't connect. Trying again in {} seconds.".format(timeout))
+            time.sleep(timeout)
+            timeout = timeout + timeout_inc
+            remaining_tries = remaining_tries - 1
+
+    raise Error()
+
 
 def get(url, path):
-	return general_request(lambda: requests.get("{}/api/{}".format(url, path)))
+    return general_request(lambda: requests.get("{}/api/{}".format(url, path)))
+
 
 def connect():
-	url = get_url()
-	print("Connecting to {}...".format(url))
-	try:
-		response = get(url, "")
-	except:
-		print("Couldn't connect.")
-		exit(1)
-	else:
-		if response.status_code == 200:
-			print("Connection successful.")
-		else:
-			print("Couldn't connect.")
-			exit(1)
+    url = get_url()
+    print("Connecting to {}...".format(url))
+    try:
+        response = get(url, "")
+    except:
+        print("Couldn't connect.")
+        exit(1)
+    else:
+        if response.status_code == 200:
+            print("Connection successful.")
+        else:
+            print("Couldn't connect.")
+            exit(1)
--- rpi/server_connect/utils.py
+++ rpi/server_connect/utils.py
@@ -2,27 +2,29 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-def check_in_config(key):
-	if key in config.keys():
-		result = config[key]
-		if result == "":
-			return None
-		else:
-			return result
-			
-	return None
-	
-def get_url():
-	hostname = check_in_config("SERVER_HOSTNAME")
-	if hostname is None:
-		hostname = check_in_config("SERVER_IP")
-		if hostname is None:
-			print("No server hostname given, stopping...")
-			exit(1)
 
-	protocol = check_in_config("HTTP_PROTOCOL")
-	if protocol is None:
-		print("No HTTP protocol given, stopping...")
-		exit(1)
-		
-	return "{}://{}".format(protocol, hostname)
+def check_in_config(key):
+    if key in config.keys():
+        result = config[key]
+        if result == "":
+            return None
+        else:
+            return result
+
+    return None
+
+
+def get_url():
+    hostname = check_in_config("SERVER_HOSTNAME")
+    if hostname is None:
+        hostname = check_in_config("SERVER_IP")
+        if hostname is None:
+            print("No server hostname given, stopping...")
+            exit(1)
+
+    protocol = check_in_config("HTTP_PROTOCOL")
+    if protocol is None:
+        print("No HTTP protocol given, stopping...")
+        exit(1)
+
+    return "{}://{}".format(protocol, hostname)
