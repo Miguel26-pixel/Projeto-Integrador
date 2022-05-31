aa05fc0c01234d78f02d310e8917146c03eb07d8
Nuno Alves <nyansword@gmail.com>
2022-05-31 22:32:18 +0100

    add: send distance data to server, simplified arduino message protocol

--- raspberry/arduino/arduino_sketch/arduino_sketch.ino
+++ raspberry/arduino/arduino_sketch/arduino_sketch.ino
@@ -96,24 +96,29 @@ void loop()
     delay(WAIT_TIME);
 
     float curTime = millis();
-
     DHTResult results = readDHT(curTime);
 
-    if (results.valid)
-    {
-        Serial.print("dht, ");
-        Serial.print(results.temperature);
-        Serial.print(", ");
-        Serial.println(results.humidity);
-    }
-
     curTime = millis();
-
     HCSRO4Result results2 = readHCSRO4(curTime);
 
-    if (results2.valid)
-    {
-        Serial.print("hcsro4, ");
+    if(!results.valid && !results2.valid) {
+        return;
+    }
+
+    if (results.valid) {
+        Serial.print("temperature: ");
+        Serial.print(results.temperature);
+        Serial.print(", humidity: ");
+        Serial.print(results.humidity);
+    }
+
+    if (results2.valid) {
+        if(results.valid) {
+            Serial.print(", ");
+        }
+        Serial.print("distance: ");
         Serial.println(results2.distance);
+    } else {
+        Serial.println();
     }
 }
--- raspberry/rpi/main.py
+++ raspberry/rpi/main.py
@@ -6,7 +6,7 @@ import logging
 
 from rpi.utils import config_logger, Config
 import rpi.server_connect.core as server_connect
-from rpi.sensors.arduino import ArduinoData, ArduinoI
+from rpi.sensors.arduino import ArduinoI
 
 
 def main(arduino: ArduinoI) -> None:
@@ -14,7 +14,7 @@ def main(arduino: ArduinoI) -> None:
     logging.info("Starting main thread on %s", socket.gethostname())
 
     poll_rate: float = Config.poll_rate
-    data_controller = server_connect.DataController()
+    data_controller = server_connect.DataController(["time", "temperature", "humidity", "distance"])
     data_controller.start()
     stopping = False
 
@@ -48,14 +48,11 @@ def main(arduino: ArduinoI) -> None:
     while not stopping:
         cur_time: float = time.time()
 
-        arduino_result: ArduinoData = arduino.get_data()
+        arduino_result = arduino.get_data()
 
         if arduino_result is not None:
-            data_controller.queue.put({
-                 'time': cur_time,
-                 'temperature': arduino_result.temperature,
-                 'humidity':  arduino_result.humidity,
-            })
+            arduino_result["time"] = cur_time
+            data_controller.queue.put(arduino_result)
 
         time.sleep(poll_rate)
 
