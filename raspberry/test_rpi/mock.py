5eafb54d879708559f7937027dde639045440496
Nuno Alves <nyansword@gmail.com>
2022-06-10 19:24:08 +0100

    fix: arduino mock

--- raspberry/test_rpi/mock.py
+++ raspberry/test_rpi/mock.py
@@ -1,8 +1,20 @@
 from random import randint
+from time import time
 from typing import Dict
 from rpi.sensors.arduino import ArduinoI
 
 
 class ArduinoMock(ArduinoI):
+    def __init__(self) -> None:
+        self.last = 0
+
     def get_data(self) -> Dict:
-        return {'temperature': randint(20, 30), 'humidity': randint(20, 80), 'distance': randint(0, 100)}
+        if time() - self.last > 2:
+            self.last = time()
+            return {
+                'temperature': randint(20, 30),
+                'humidity': randint(20, 80),
+                'distance': randint(0, 100)
+            }
+
+        return None
