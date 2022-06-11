ead6a2da0d00ca1d224b99838829a70ddf104932
retchut <retchut@protonmail.ch>
2022-06-11 21:38:22 +0100

    setup prisma migration

--- plants-app/prisma/migrations/20220611203614_init/migration.sql
+++ plants-app/prisma/migrations/20220611203614_init/migration.sql
@@ -1,5 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
-
 -- CreateTable
 CREATE TABLE "RasperryPI" (
     "id" SERIAL NOT NULL,
@@ -12,14 +10,21 @@ CREATE TABLE "RasperryPI" (
 -- CreateTable
 CREATE TABLE "Plant" (
     "id" SERIAL NOT NULL,
+    "piID" INTEGER NOT NULL,
+
+    CONSTRAINT "Plant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PlantData" (
     "time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "temperature" DOUBLE PRECISION NOT NULL,
     "light" DOUBLE PRECISION NOT NULL,
     "humidity" DOUBLE PRECISION NOT NULL,
     "gasConc" DOUBLE PRECISION NOT NULL,
-    "piID" INTEGER NOT NULL,
+    "plantId" INTEGER NOT NULL,
 
-    CONSTRAINT "Plant_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "PlantData_pkey" PRIMARY KEY ("time")
 );
 
 -- CreateIndex
@@ -28,5 +33,9 @@ CREATE UNIQUE INDEX "Plant_id_key" ON "Plant"("id");
 -- AddForeignKey
 ALTER TABLE "Plant" ADD CONSTRAINT "Plant_piID_fkey" FOREIGN KEY ("piID") REFERENCES "RasperryPI"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
--- Create time hypertable
-SELECT create_hypertable('"Plant"', 'time');
+-- AddForeignKey
+ALTER TABLE "PlantData" ADD CONSTRAINT "PlantData_plantId_fkey" FOREIGN KEY ("plantId") REFERENCES "Plant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- set up timescaledb
+CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
+SELECT create_hypertable('"PlantData"', 'time');
