b5b05388aae455a5cdfa63cfcca204861bed5be8
Nuno Alves <nyansword@gmail.com>
2022-06-11 23:32:11 +0100

    add: line chart and update on pusher

--- my-chakra-app/pages/plantCard.js
+++ my-chakra-app/pages/plantCard.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import { useRouter } from 'next/router';
-import {useState, useEffect} from 'react';
+import { useEffect } from 'react';
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
@@ -20,201 +19,246 @@ import List from '@mui/material/List';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import { mainListItems, secondaryListItems } from './listItems';
-import { green, purple } from '@mui/material/colors';
+import { purple } from '@mui/material/colors';
 import MuiDrawer from '@mui/material/Drawer';
 import MenuIcon from '@mui/icons-material/Menu';
-import Paper from '@mui/material/Paper';
 import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Spritesheet from 'react-responsive-spritesheet';
-import SaveIcon from '@mui/icons-material';
 import Pusher from 'pusher-js';
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
 
 function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
 
 const mdTheme = createTheme({
-  status: {
-    danger: '#e53e3e',
-  },
-  palette: {
-    primary: {
-      main: '#2e7d32',
-      darker: '#1b5e20',
+    status: {
+        danger: '#e53e3e',
     },
-    neutral: {
-      main: purple[500],
+    palette: {
+        primary: {
+            main: '#2e7d32',
+            darker: '#1b5e20',
+        },
+        neutral: {
+            main: purple[500],
+        },
     },
-  },
 });
 
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    '& .MuiDrawer-paper': {
-      position: 'relative',
-      whiteSpace: 'nowrap',
-      width: drawerWidth,
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      boxSizing: 'border-box',
-      ...(!open && {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-          width: theme.spacing(9),
+    ({ theme, open }) => ({
+        '& .MuiDrawer-paper': {
+            position: 'relative',
+            whiteSpace: 'nowrap',
+            width: drawerWidth,
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            boxSizing: 'border-box',
+            ...(!open && {
+                overflowX: 'hidden',
+                transition: theme.transitions.create('width', {
+                    easing: theme.transitions.easing.sharp,
+                    duration: theme.transitions.duration.leavingScreen,
+                }),
+                width: theme.spacing(7),
+                [theme.breakpoints.up('sm')]: {
+                    width: theme.spacing(9),
+                },
+            }),
         },
-      }),
-    },
-  }),
+    }),
 );
 
 const drawerWidth = 240;
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+    shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
+    zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
     }),
-  }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
 }));
 
+
+const options = {
+    scales: {
+        y: {
+            type: 'linear'
+        },
+        x: {
+            type: 'linear'
+        }
+    }
+}
+
+
 export default function PlantCard() {
-  const router = useRouter();
-  const [open, setOpen] = React.useState(false);
-  const toggleDrawer = () => {
-    setOpen(!open);
-  };
+    const router = useRouter();
+    const [open, setOpen] = React.useState(false);
+    const [temperature, setTemperature] = React.useState([
+        { x: 1, y: 10 },
+        { x: 2, y: 2 },
+        { x: 3, y: 5 },
+        { x: 5, y: 22 },
+        { x: 10, y: 17 },
+        { x: 15, y: 11 },
+        { x: 17, y: 3 }
+    ])
 
-  const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY, {
-    cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
-    encrypted: true
-  });
+    useEffect(() => {
+        const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY, {
+            cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
+            useTLS: false
+        });
 
-  console.log(process.env);
+        const channel = pusher.subscribe('plant-channel');
 
-  const channel = pusher.subscribe('plant-channel');
-  
-  useEffect(() => {
-      channel.bind('plant-channel', function(data) {
-              alert(JSON.stringify(data));
-              });  
-  },[]);
+        channel.bind("new-data", function (data) {
+            setTemperature([...temperature, { x: 2, y: 2 }])
+            console.log(data)
+        });
 
-  return (
+        return () => {
+            channel.unbind('new-data');
+            pusher.unsubscribe('plant-channel')
+        }
+    }, []);
 
-    <><Head>
-    <title>GREENSTONE</title>
-    <link rel="icon" type="image/x-icon" href="/plant.ico"></link>
-    </Head>
-    <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar position="absolute" open={open}>
-          <Toolbar
-            sx={{
-              pr: '24px', // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: '36px',
-                ...(open && { display: 'none' }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              GREENSTONE
-            </Typography>
-            <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <List component="nav">
-            {mainListItems}
-            <Divider sx={{ my: 1 }} />
-            {secondaryListItems}
-          </List>
-        </Drawer>
-        <Box
-          sx={{
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-          }}
-        >
-          <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Stack
-              direction="row"
-              justifyContent="flex-end"
-              alignItems="center"
-              spacing={2}
-            >
-            </Stack>
-            <Grid container spacing={3}
-                direction="row"
-                justifyContent="center"
-                alignItems="center">
-              {/* Recent Orders */}
-            {/* <Grid item >
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <><Head>
+            <title>GREENSTONE</title>
+            <link rel="icon" type="image/x-icon" href="/plant.ico"></link>
+        </Head>
+            <ThemeProvider theme={mdTheme}>
+                <Box sx={{ display: 'flex' }}>
+                    <CssBaseline />
+                    <AppBar position="absolute" open={open}>
+                        <Toolbar
+                            sx={{
+                                pr: '24px', // keep right padding when drawer closed
+                            }}
+                        >
+                            <IconButton
+                                edge="start"
+                                color="inherit"
+                                aria-label="open drawer"
+                                onClick={toggleDrawer}
+                                sx={{
+                                    marginRight: '36px',
+                                    ...(open && { display: 'none' }),
+                                }}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Typography
+                                component="h1"
+                                variant="h6"
+                                color="inherit"
+                                noWrap
+                                sx={{ flexGrow: 1 }}
+                            >
+                                GREENSTONE
+                            </Typography>
+                            <IconButton color="inherit">
+                                <Badge badgeContent={4} color="secondary">
+                                    <NotificationsIcon />
+                                </Badge>
+                            </IconButton>
+                        </Toolbar>
+                    </AppBar>
+                    <Drawer variant="permanent" open={open}>
+                        <Toolbar
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'flex-end',
+                                px: [1],
+                            }}
+                        >
+                            <IconButton onClick={toggleDrawer}>
+                                <ChevronLeftIcon />
+                            </IconButton>
+                        </Toolbar>
+                        <Divider />
+                        <List component="nav">
+                            {mainListItems}
+                            <Divider sx={{ my: 1 }} />
+                            {secondaryListItems}
+                        </List>
+                    </Drawer>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            height: '100vh',
+                            overflow: 'auto',
+                        }}
+                    >
+                        <Toolbar />
+                        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                            <Stack
+                                direction="row"
+                                justifyContent="flex-end"
+                                alignItems="center"
+                                spacing={2}
+                            >
+                            </Stack>
+                            <Grid container spacing={3}
+                                direction="row"
+                                justifyContent="center"
+                                alignItems="center">
+                                {/* Recent Orders */}
+                                {/* <Grid item >
             <Spritesheet
                 className={`my-element__class--style`}
                 image={`https://raw.githubusercontent.com/danilosetra/react-responsive-spritesheet/master/assets/images/examples/sprite-image-horizontal.png`}
@@ -227,35 +271,31 @@ export default function PlantCard() {
                 loop={true}
               />
                 </Grid> */}
-                <Grid item>
-                        <Card sx={{ maxWidth: "40%", mx : "auto" }}>
-                                <CardMedia
-                                component="img"
-                                alt="green iguana"
-                                height="140"
-                                image="/static/images/cards/contemplative-reptile.jpg" />
-                                <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                Plant 1
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000
-                                species, ranging across all continents except Antarctica
-                                </Typography>
-                                </CardContent>
-                                <CardActions>
-                        <Button size="small" onClick={() => router.push('/dashboard')}>See dashboard</Button>
-                        <Button size="small" onClick={() => router.push('/experimentList')}>Edit information</Button>
-                        </CardActions>
-                        </Card>
-                </Grid>
-            </Grid>
-            <Copyright sx={{ pt: 4 }} />
-          </Container>
-        </Box>
-      </Box>
-    </ThemeProvider></>
+                                <Grid item xs={12}>
+                                    <Card>
+                                        <CardContent>
+                                            <Line data={{
+                                                datasets: [
+                                                    {
+                                                        label: "Temperature",
+                                                        backgroundColor: 'rgb(255, 99, 132)',
+                                                        borderColor: 'rgb(255, 99, 132)',
+                                                        lineTension: 0.1,
+                                                        data: temperature,
+                                                        fill: false,
+                                                    },
+                                                ]
+                                            }} options={options} />
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                            </Grid>
+                            <Copyright sx={{ pt: 4 }} />
+                        </Container>
+                    </Box>
+                </Box>
+            </ThemeProvider></>
     )
-  }
+}
 
 
