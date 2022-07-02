a8eab245b94c29a05500cf25958a2541ba26dbfe
retchut <retchut@protonmail.ch>
2022-07-02 12:55:36 +0100

    feat: add experimental pi creation post route

--- my-chakra-app/pages/api/create/pi.js
+++ my-chakra-app/pages/api/create/pi.js
@@ -6,6 +6,41 @@ export default async (req, res) => {
     }
 
     try{
+        const piData = req.body.piData;
+        const piName = piData.hostname;
+        const piIP = piData.ip;
+        for (var plant : piData.data){
+            const editPlantID = parseInt(plant.plant);
+            const plantExists = await prisma.$exists.PLANT({
+                id : editPlantID;
+            })
+            if (!plantExists){
+                // TODO: create new plant after Nuno changes the packet contents
+                res.status(400).json( message: "That plant does not exist" );
+            }
+            
+            const updatedPlant = await prisma.PLANT.update({
+                where: {
+                    id : editPlantID
+                },
+                data : {
+                    
+                    plantdata : {
+                        create : [
+                            {
+                                time : plant.time,
+                                temperature : plant.temperature,
+                                humidity : plant.humidity,
+                                // distance : plant.distance
+                                plantID : editPlantID
+                            }
+                        ]
+                    }
+                }
+            })
+
+        }
+
         const newPi = await prisma.pi.create({
             data : {
                 piName : req.body.piName,
