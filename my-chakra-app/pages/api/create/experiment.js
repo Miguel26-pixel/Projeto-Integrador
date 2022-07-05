596b91e8f9059687e31fbe38d54741dfc1c957ee
Nuno Alves <nyansword@gmail.com>
2022-07-05 23:51:36 +0100

    res

--- my-chakra-app/pages/api/create/experiment.js
+++ my-chakra-app/pages/api/create/experiment.js
@@ -2,7 +2,7 @@ import prisma from "../../../db";
 
 export default async (req, res) => {
     if (req.method !== 'POST'){
-        return req.status(405).json({ message: 'Method not allowed'});
+        return res.status(405).json({ message: 'Method not allowed'});
     }
 
     try{
