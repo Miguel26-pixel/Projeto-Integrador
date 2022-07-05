1eeaa98844f071fccbfb975e9d1406e750b99bb8
Miguel Amorim <migamorim2012@gmail.com>
2022-07-05 11:53:00 +0100

    Edit form error

--- my-chakra-app/pages/experimentsList.js
+++ my-chakra-app/pages/experimentsList.js
@@ -23,7 +23,9 @@ import {FormControl, InputLabel, Input, FormHelperText} from '@mui/material';
 function ExperimentListPage() {
     const router = useRouter();
     const [openf, setOpenf] = useState(false);
+    const [openEdit, setOpenEdit] = useState(false);
     const [experiments, setExperiments] = useState(<></>)
+    const [edit, setEdit] = useState(null);
 
     const handleClickOpen = () => {
         setOpenf(true);
@@ -33,9 +35,18 @@ function ExperimentListPage() {
         setOpenf(false);
     };
 
+    async function handleClickOpenEdit() {
+        setOpenEdit(true);
+        console.log("ola");
+    };
+
+    const handleCloseEdit = () => {
+        setOpenEdit(false);
+    };
+
     useEffect(() => {
         async function fetchData() {
-            let data = await fetcher(window.location.origin + "/api/experiments", null)
+            let data = await fetcher(window.location.origin + "/api/experiments", null);
 
             setExperiments(data.map((val) =>
                 <Grid item xs={12} md={4} lg={4} key={val.id}>
@@ -55,6 +66,40 @@ function ExperimentListPage() {
                         </CardContent>
                         <CardActions>
                             <Button size="small" onClick={() => router.push('/experiment/' + val.id)}>View Plants</Button>
+                            <Button size="small" onClick={handleClickOpenEdit}>Edit Experiment</Button>
+
+                            <Dialog
+                                open={openEdit}
+                                onClose={handleCloseEdit}
+                                fullWidth
+                                maxWidth = 'lg'
+                                className='popup-form'
+                                >
+                                <form action={"/api/create/experiment/"} method="POST" className="flex flex-col">
+                                    <fieldset>
+                                        <legend>Edit Experiment</legend>
+                                    
+                                    <InputLabel htmlFor="name">Name</InputLabel>
+                                    <Input id="exp-name" aria-describedby="my-helper-name" defaultValue={val.name} />
+
+                                    <InputLabel htmlFor="info">More info</InputLabel>
+                                    <textarea id="my-exp-info" aria-describedby="my-helper-info" defaultValue={val.info}></textarea>
+                                    
+                                    <InputLabel htmlFor="image">Image</InputLabel>
+                                    <Input type="file" id="my-input" aria-describedby="my-helper-text" />
+                                    
+                                    <div>
+                                    <Input
+                                        type="submit"
+                                        className="px-4 py-4 font-bold text-white hover:bg-green-700"
+                                    >
+                                        Submit
+                                    </Input>
+                                    </div>
+                                    </fieldset>
+                                </form>
+                            </Dialog>
+
                         </CardActions>
                     </Card>
                 </Grid>
